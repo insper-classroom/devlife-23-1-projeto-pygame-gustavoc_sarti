2a0c1470f295b6cc7d0c1838a6a8ab15adaab333
Gubscruz <124577208+Gubscruz@users.noreply.github.com>
2023-04-22 10:14:20 -0300

    sei la

--- game.py
+++ game.py
@@ -1,15 +1,16 @@
 import pygame
-from sprites.player import Player
+from sprites.player import *
 #Class principal do jogo que opera as 3 funções principais do Pygame:
 #Criar seu estado inicial, atualizar ele e desenhar na tela.
 class Game:
     def __init__(self):
         pygame.init()
         self.window = pygame.display.set_mode((1024, 720), vsync=True, flags=pygame.SCALED)
-        pygame.display.set_caption('Oi')
+        pygame.display.set_caption('')
         #Cria dicionario de assets para serem utilizados
+        player = pygame.image.load('images/square.png')
         self.assets = {
-            'player': pygame.image.load('images/square.png')
+            'player': pygame.transform.scale(player, (50, 50))
         }
 
         #Cria dicionario de state(infos diversas) a serem utilizados
--- sprites/player.py
+++ sprites/player.py
@@ -1,8 +1,22 @@
 import pygame
+from enum import Enum
 
-class Player(pygame.sprite.Sprite):
-    def __init__(self):
-        pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.image.load('images/square.png')
+
+class Direction(Enum):
+    UP = 0
+    DOWN = 1
+    LEFT = 2
+    RIGHT = 3
+
+
+class Player:
+    direction = None
+    speed = 0 
+    bounds = None
+
+    def __init__(self, bounds):
+        self.bounds = bounds
         self.retangulo = self.image.get_rect()
-        
+    
+    def draw(self, window):
+        window.blit(self.image, self.retangulo)
