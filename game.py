e8c74033ca5b8b50c730eb0e5e64fef85ce987cd
Gubscruz <124577208+Gubscruz@users.noreply.github.com>
2023-04-25 14:06:57 -0300

    novas paredes

--- game.py
+++ game.py
@@ -1,7 +1,7 @@
 import pygame
 from sprites.player import *
 from config import *
-from map_content import Wall
+from sprites.map_content import *
 
 
 class Game:
@@ -28,15 +28,17 @@ class Game:
                 y = line_index  * WALL_GAP
                 if column == 'X':
                     Wall((x, y), self.sprites)
+                if column == 'S':
+                    SideWall((x, y), self.sprites)
+                # if column == ' ':
+                #     Floor((x, y), self.sprites)
                 if column == 'p':
                     self.player = Player((x, y), self.sprites)
-                    
 
     def desenha(self):         
         self.sprites.draw(self.window)           
         pygame.display.update()  
         
-
     def start(self):
         while self.atualiza_estado():
             self.desenha()
--- sprites/map_content.py
+++ sprites/map_content.py
@@ -1,10 +1,23 @@
 import pygame
 
+class SideWall(pygame.sprite.Sprite):
+    def __init__(self, position, group):
+        super().__init__(group)
+        self.image = pygame.image.load('assets/images/sidewall.png')
+        self.image = pygame.transform.scale(self.image, (20, 25))
+        self.rect = self.image.get_rect(topleft=position)
+    
 class Wall(pygame.sprite.Sprite):
     def __init__(self, position, group):
-
         super().__init__(group)
-        self.image = pygame.image.load('assets/images/parede.png')
+        self.image = pygame.image.load('assets/images/wall.png')
         self.image = pygame.transform.scale(self.image, (25, 25))
         self.rect = self.image.get_rect(topleft=position)
     
+class Floor(pygame.sprite.Sprite):
+    def __init__(self, position, group):
+        super().__init__(group)
+        self.image = pygame.image.load('assets/images/HIGHTECH.png')
+        self.image = pygame.transform.scale(self.image, (25, 25))
+        self.image.set_alpha(5)
+        self.rect = self.image.get_rect(topleft=position)
