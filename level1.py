912ed9b15151beb96545e2fc9d3c22a900d422a6
joaopgs4 <joaopgs4@al.insper.edu.br>
2023-05-01 15:54:45 -0300

    Brief code clean and organization. No mechanics change

--- level1.py
+++ level1.py
@@ -4,6 +4,7 @@ from config import *
 from sprites.map_content import *
 import gameover
 import level2
+import functions
 
 #Timer
 class Timer:
@@ -60,30 +61,8 @@ class Level1:
             if event.type == pygame.QUIT:
                 pygame.quit()
 
-        lasered_x = pygame.sprite.groupcollide(self.players, self.lasers_x, False, False, pygame.sprite.collide_rect)
-        lasered_y = pygame.sprite.groupcollide(self.players, self.lasers_y, False, False, pygame.sprite.collide_rect)
-        if lasered_x or lasered_y:
-                self.defeat = True
-        
-
-        #Codigo para a remoção dos LASER disparados, feito com auxilio do chatGPT para criar a base e pensar a lõgica.
-        #Basicamente ao remover a arma que dispara o laser, tambem pega suas cordenadas(na tela Pygame), as divide pelo
-        #tamanho de um 'tile' do mapa, assim descobrindo a posição X,Y do arma(na lista do mapa). Em seguida remove todos
-        #os lasers desta fileira ou coluna, configurado manualmente dependendo se é um laser X ou Y.
-        lasers_colididos = pygame.sprite.groupcollide(self.players, self.guns, False, True, pygame.sprite.collide_rect)
-        for player_sprite, laser_list in lasers_colididos.items():
-            for gun_sprite in laser_list:
-                #Pega tanto os index X e Y de colisao
-                col_index = gun_sprite.rect.x // 40  #40 por ser o tamanho de um tile na tela
-                row_index = gun_sprite.rect.y // 40  #40 por ser o tamanho de um tile na tela
-                #Remove todos os laser de armas destruidas
-                for arma_vertical in self.lasers_y:
-                    if arma_vertical.rect.x // 40 == col_index:
-                        self.lasers_y.remove(arma_vertical)
-                for arma_horizontal in self.lasers_x:
-                    if arma_horizontal.rect.y // 40 == row_index:
-                        self.lasers_x.remove(arma_horizontal)
-
+        functions.player_hit(self)
+        functions.laser_break(self)
 
         return True
     
