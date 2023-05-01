4455f41366ba6f5988094b4736f3ee2610154050
joaopgs4 <joaopgs4@al.insper.edu.br>
2023-05-01 15:23:41 -0300

    Win screen complete

--- win.py
+++ win.py
@@ -3,27 +3,31 @@ import pygame
 
 class Win:
     def __init__(self):
+        pygame.init()
         self.window = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-        self.button = pygame.image.load('assets/images/menu/button_unselected.jpg')
+        self.button = pygame.image.load('assets/images/menu/button_selected.jpg')
         self.button = pygame.transform.scale(self.button, (240, 80))
-        self.botoes = ['restart', 'sair']
+        self.botoes = ['sair']
         self.fonte_padrao = pygame.font.get_default_font()
         self.fonte = pygame.font.Font(self.fonte_padrao, 45)
-        self.titulo = self.fonte.render('Game Over', True, (255, 255, 255))
+        self.titulo = self.fonte.render('Você Escapou!', True, (0, 0, 0))
+        #Configura a imagem de fundo
+        background = pygame.image.load('assets/images/menu/win.png')
+        self.background = pygame.transform.scale(background,(1420,969))
 
-        self.loss_sound = pygame.mixer.Sound('assets/sounds/sounds_misc/ogg/defeat.ogg')
-        self.loss_sound.set_volume(0.1)
+        self.win_sound = pygame.mixer.Sound('assets/sounds/sounds_misc/ogg/victory_music.ogg')
+        self.win_sound.set_volume(0.1)
 
 
-    def desenha_gameover(self):
-        self.window.fill((0, 0, 0))
+    def desenha_win(self):
+        self.window.blit(self.background, (0, 0))
         self.window.blit(self.titulo, ((SCREEN_WIDTH - self.titulo.get_width()) // 2, 100))
         self.button_width, self.button_height = self.button.get_size()
         for i, label in enumerate(self.botoes):
             text_surface = self.fonte.render(label, True, (255, 255, 255))
             text_width, text_height = text_surface.get_size()
             self.x_button = (SCREEN_WIDTH - self.button_width) // 2
-            self.y_button = 300 + (i * (self.button_height + 50))
+            self.y_button = 300 + (self.button_height + 50)
             y_text = self.y_button + (self.button_height - text_height) // 2
             self.window.blit(self.button, (self.x_button, self.y_button))
             self.window.blit(text_surface, ((SCREEN_WIDTH - text_width) // 2, y_text))
@@ -31,22 +35,7 @@ class Win:
         # self.window.blit(self.resultado)
         pygame.display.update()
 
-    def clique_jogar_novamente(self):
-        self.x_button = (SCREEN_WIDTH - self.button_width) // 2
-        self.y_button = (SCREEN_HEIGHT - (len(self.botoes) * (self.button_height + 50))) // 2
-        if (
-            self.x_button <= self.mouse_pos[0] and 
-            self.mouse_pos[0] <= self.x_button + self.button_width and
-            self.y_button <= self.mouse_pos[1] and
-            self.mouse_pos[1] <= self.y_button + self.button_height
-            ):
-                return True
-        else:
-                return False
-
     def clique_sair(self):
-        self.x_button = (SCREEN_WIDTH - self.button_width) // 2
-        self.y_button = (SCREEN_HEIGHT - (len(self.botoes) * (self.button_height + 50))) // 2 + self.button_height + 50
         if (
             self.x_button <= self.mouse_pos[0] and 
             self.mouse_pos[0] <= self.x_button + self.button_width and
@@ -54,26 +43,20 @@ class Win:
             self.mouse_pos[1] <= self.y_button + self.button_height
             ):
                 return True
-        else:
-                return False
 
     def atualiza_estado(self):
         self.mouse_pos = pygame.mouse.get_pos()
-        self.desenha_gameover()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 exit()
             if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
-                if self.clique_jogar_novamente():
-                    self.reset = True
                 if self.clique_sair():
                     pygame.quit()
-                    exit()
         return True
 
     def start(self):
         pygame.mixer.music.stop()
-        self.loss_sound.play()
+        self.win_sound.play()
         while self.atualiza_estado():
-            self.desenha_gameover()
+            self.desenha_win()
